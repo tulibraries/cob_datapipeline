5fb4241322af7845b8bf6497660a772e3abfc78e
Christina Harlow <cmharlow@gmail.com>
2019-03-21 18:47:45 -0400

    read through of file renaming script

--- renamemarcfiles.py
+++ renamemarcfiles.py
@@ -1,20 +1,18 @@
-from airflow import DAG
-from airflow.models import Variable
-from airflow import AirflowException
-import os
-import datetime
-import time
+"""TU Libraries Airflow Script for Renaming OAI Harvest Files after Done."""
 
-def renamemarcfiles_onsuccess(ds, **kwargs):
-    outfile = None
-    deletedfile = None
-    date_current_harvest = datetime.datetime.now()
+import os
+import time
+from airflow.models import Variable
+
+#pylint: disable=missing-docstring,invalid-name, C0301
+def renamemarcfiles_onsuccess():
+    """Method to Rename OAI Harvest Records Files after Processing."""
     date = Variable.get("almaoai_last_harvest_date")
 
     outfilename = Variable.get("AIRFLOW_DATA_DIR") + '/oairecords.xml'
     if os.path.isfile(outfilename):
-        os.rename(outfilename, Variable.get("AIRFLOW_DATA_DIR") + '/oairecords-{}-{}.xml'.format(date,time.time()))
-        
+        os.rename(outfilename, Variable.get("AIRFLOW_DATA_DIR") + '/oairecords-{}-{}.xml'.format(date, time.time()))
+
     deletedfilename = Variable.get("AIRFLOW_DATA_DIR") + '/oairecords_deleted.xml'
     if os.path.isfile(deletedfilename):
-        os.rename(deletedfilename, Variable.get("AIRFLOW_DATA_DIR") + '/oairecords_deleted-{}-{}.xml'.format(date,time.time()))
+        os.rename(deletedfilename, Variable.get("AIRFLOW_DATA_DIR") + '/oairecords_deleted-{}-{}.xml'.format(date, time.time()))

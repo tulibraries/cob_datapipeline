5427affa16170e6e166109ac98dbdd575f9bfd97
Christina Harlow <cmharlow@gmail.com>
2019-03-21 18:44:11 -0400

    read through of alma stfp harvest task

--- task_almasftp.py
+++ task_almasftp.py
@@ -1,13 +1,14 @@
-from airflow import DAG
-from airflow import AirflowException
-from airflow.models import Variable
+"""TU Libraries Airflow Task for Alma SFTP Server Harvest."""
+
 from airflow.operators.python_operator import PythonOperator
-import os
 from cob_datapipeline.almasftp_fetch import almasftp_fetch
 
 def task_almasftp(dag):
+    """Method to kick off almasftp.py script for Alma SFTP Record Harvest."""
+    #pylint: disable=missing-docstring,invalid-name, C0301
+
     t1 = PythonOperator(
-        task_id= 'almasftp',
+        task_id='almasftp',
         python_callable=almasftp_fetch,
         dag=dag
     )

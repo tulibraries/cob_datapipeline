e52161df568901a70240925faf8395c9b8ccecec
Christina Harlow <cmharlow@gmail.com>
2019-03-21 18:38:14 -0400

    read through of index (create / update) marc task

--- task_ingestmarc.py
+++ task_ingestmarc.py
@@ -1,25 +1,21 @@
-from airflow import DAG
+"""TU Libraries Airflow Task for Alma to Solr Record Creates & Updates."""
+
+import os
 from airflow.operators.bash_operator import BashOperator
 from airflow.models import Variable
-from airflow import AirflowException
-import os
-import datetime
-import time
 
 def ingest_marc(dag):
-    outfile = None
-    deletedfile = None
-    date_current_harvest = datetime.datetime.now()
-    date = Variable.get("almaoai_last_harvest_date")
+    """Method to Index (create and update) records from Alma into Solr."""
+    #pylint: disable=missing-docstring,invalid-name, C0301
+
     outfilename = Variable.get("AIRFLOW_DATA_DIR") + '/oairecords.xml'
     ingest_command = Variable.get("AIRFLOW_HOME") + "/dags/cob_datapipeline/scripts/ingest_marc.sh"
     if os.path.isfile(ingest_command):
-       t1 = BashOperator(
-            task_id= 'ingest_marc',
+        t1 = BashOperator(
+            task_id='ingest_marc',
             bash_command="{} {}".format(ingest_command, outfilename)+' ',
             dag=dag
-       )
-
+            )
     else:
         raise Exception(str(os.path.isfile(ingest_command)) + " Cannot locate {}".format(ingest_command))
     return t1

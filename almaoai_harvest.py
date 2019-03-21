598c42001f0d1941060d53d32b928e2b22d1e6e6
Christina Harlow <cmharlow@gmail.com>
2019-03-21 19:16:17 -0400

    read through alma oai harvest script

--- almaoai_harvest.py
+++ almaoai_harvest.py
@@ -1,13 +1,17 @@
+"""TU Libraries Airflow Script for Harvesting Records from Alma OAI Endpoint."""
+
 import datetime
-import time
-from sickle import Sickle
-from airflow.models import Variable
-from airflow import AirflowException
 import xml.etree.ElementTree
 import xml.dom.minidom
 import os.path
+from sickle import Sickle
+from airflow.models import Variable
+from airflow import AirflowException
 
+#pylint: disable=R0914,R0915,R1705
 def almaoai_harvest():
+    #pylint: disable=missing-docstring,invalid-name, C0301
+    """Method to harvest Alma records from Alma's OAI-PMH HTTP Endpoint."""
     try:
         data_dir = Variable.get("AIRFLOW_DATA_DIR")
         outfile = None
@@ -18,26 +22,26 @@ def almaoai_harvest():
         try:
             date = Variable.get("almaoai_last_harvest_date")
         except KeyError:
-            Variable.set("almaoai_last_harvest_date", date_current_harvest.strftime('%Y-%m-%d'))
+            Variable.set("almaoai_last_harvest_date", date_current_harvest.strftime("%Y-%m-%d"))
             date = date_current_harvest
 
-        outfilename = data_dir + '/oairecords.xml'
+        outfilename = data_dir + "/oairecords.xml"
         if os.path.isfile(outfilename):
-            print('Not re-harvesting until ingest_marc succeeds and moves old oairecords.xml.')
+            print("Not re-harvesting until ingest_marc succeeds and moves old oairecords.xml.")
             return
         else:
-            outfile = open(outfilename,'w')
+            outfile = open(outfilename, "w")
 
-        deletedfilename = data_dir + '/oairecords_deleted.xml'
-        deletedfile = open(deletedfilename,'w')
+        deletedfilename = data_dir + "/oairecords_deleted.xml"
+        deletedfile = open(deletedfilename, "w")
 
-        endpoint_url = 'https://temple.alma.exlibrisgroup.com/view/oai/01TULI_INST/request'
+        endpoint_url = "https://temple.alma.exlibrisgroup.com/view/oai/01TULI_INST/request"
         sickle = Sickle(endpoint_url)
-        records = sickle.ListRecords(**{'metadataPrefix':'marc21','set':'blacklight','from':'{}'.format(date)})
+        records = sickle.ListRecords(**{"metadataPrefix":"marc21", "set":"blacklight", "from":"{}".format(date)})
         # xml.etree.ElementTree.register_namespace('xmlns','http://www.loc.gov/MARC21/slim')
         newfirstline = '<?xml version="1.0" encoding="UTF-8"?>'
         newroot = '<collection xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">'
-        newrootclosingtag = '</collection>'
+        newrootclosingtag = "</collection>"
         outfile.write(newfirstline)
         outfile.write(newroot)
         deletedfile.write(newfirstline)
@@ -47,21 +51,21 @@ def almaoai_harvest():
             # outfile.write(xml.etree.ElementTree.tostring(subrecord, encoding='unicode'))
             header = tree[0]
             subrecord = None
-            if len(list(tree)) > 1 and len(list(tree[1])) > 0:
+            if len(list(tree)) > 1 and list(tree[1]):
                 subrecord = tree[1][0]
             if subrecord is not None:
                 # if subrecord.get('status') == 'deleted':
                 #     deletedfile.write('<record>{}</record>'.format(xml.etree.ElementTree.tostring(header, encoding='unicode')))
                 # else:
-                subrecord.insert(0,header)
-                #outfile.write(xml.dom.minidom.parseString(xml.etree.ElementTree.tostring(subrecord,'unicode')).toprettyxml(indent="\t"))
-                outfile.write(xml.etree.ElementTree.tostring(subrecord, encoding='unicode'))
-                num_updated_recs+=1
-            elif header.get('status') == 'deleted':
-                deletedfile.write('<record>{}</record>'.format(xml.etree.ElementTree.tostring(header, encoding='unicode')))
-                num_deleted_recs+=1
+                subrecord.insert(0, header)
+                # outfile.write(xml.dom.minidom.parseString(xml.etree.ElementTree.tostring(subrecord,'unicode')).toprettyxml(indent="\t"))
+                outfile.write(xml.etree.ElementTree.tostring(subrecord, encoding="unicode"))
+                num_updated_recs += 1
+            elif header.get("status") == "deleted":
+                deletedfile.write("<record>{}</record>".format(xml.etree.ElementTree.tostring(header, encoding="unicode")))
+                num_deleted_recs += 1
             else:
-                print('subrecord issue?')
+                print("subrecord issue?")
                 print(r.raw)
         outfile.write(newrootclosingtag)
         outfile.close()
@@ -69,11 +73,11 @@ def almaoai_harvest():
         deletedfile.close()
         Variable.set("almaoai_last_num_oai_update_recs", num_updated_recs)
         Variable.set("almaoai_last_num_oai_delete_recs", num_deleted_recs)
-        Variable.set("almaoai_last_harvest_date", date_current_harvest.strftime('%Y-%m-%d'))
+        Variable.set("almaoai_last_harvest_date", date_current_harvest.strftime("%Y-%m-%d"))
     except:
         if outfile is not None and outfile.closed is not True:
             outfile.close()
         if deletedfile is not None and deletedfile.closed is not True:
             deletedfile.close()
         print(r.raw)
-        raise AirflowException('Harvest failed.')
+        raise AirflowException("Harvest failed.")

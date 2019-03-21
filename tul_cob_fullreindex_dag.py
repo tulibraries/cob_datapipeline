8b0eaa3231dce9b4cf7549a36c27de2333c77879
Christina Harlow <cmharlow@gmail.com>
2019-03-21 18:07:25 -0400

    read through of ftp full index dag

--- tul_cob_fullreindex_dag.py
+++ tul_cob_fullreindex_dag.py
@@ -1,18 +1,17 @@
-import airflow
-from airflow import utils
+"""TUL_FULL_INDEX: TU Libraries Airflow DAG for Full Alma to Solr Indexing."""
+
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models import Variable
-from datetime import datetime, timedelta
 from airflow.operators.http_operator import SimpleHttpOperator
-from airflow.operators.python_operator import PythonOperator
 from cob_datapipeline.task_almasftp import task_almasftp
 
-core_name = Variable.get("BLACKLIGHT_CORE_NAME");
+CORE_NAME = Variable.get("BLACKLIGHT_CORE_NAME")
 
-param_endpoint_replication = '/solr/' + core_name + '/replication'
-param_endpoint_update = '/solr/' + core_name + 'update'
+PARAM_ENDPOINT_REPLICATION = '/solr/' + CORE_NAME + '/replication'
+PARAM_ENDPOINT_UPDATE = '/solr/' + CORE_NAME + 'update'
 
-default_args = {
+DEFAULT_ARGS = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2018, 12, 13),
@@ -23,54 +22,54 @@ default_args = {
     'retry_delay': timedelta(minutes=10),
 }
 
-dag = DAG(
-    'tul_cob_reindex', default_args=default_args, schedule_interval=None)
+FTP_INDEX_DAG = DAG(
+    'tul_cob_reindex', default_args=DEFAULT_ARGS, schedule_interval=None)
 
 
-almasftp_task = task_almasftp(dag)
+ALMASFTP_TASK = task_almasftp(FTP_INDEX_DAG)
 
 #http://master_host:port/solr/core_name/replication?command=disablereplication
-pause_replication = SimpleHttpOperator(
+PAUSE_REPLICATION = SimpleHttpOperator(
     task_id='pause_replication',
     method='GET',
     http_conn_id='AIRFLOW_CONN_SOLR_LEADER',
-    endpoint=param_endpoint_replication,
+    endpoint=PARAM_ENDPOINT_REPLICATION,
     data={"command": "disablereplication"},
     headers={},
-    dag=dag)
+    dag=FTP_INDEX_DAG)
 
 #http://master_host:port/solr/core_name/replication?command=enablereplication
-resume_replication = SimpleHttpOperator(
+RESUME_REPLICATION = SimpleHttpOperator(
     task_id='resume_replication',
     method='GET',
     http_conn_id='AIRFLOW_CONN_SOLR_LEADER',
-    endpoint=param_endpoint_replication,
+    endpoint=PARAM_ENDPOINT_REPLICATION,
     data={"command": "enablereplication"},
     trigger_rule="all_done",
     headers={},
-    dag=dag)
+    dag=FTP_INDEX_DAG)
 
 
-clear_index = SimpleHttpOperator(
+CLEAR_INDEX = SimpleHttpOperator(
     task_id='clear_index',
     method='GET',
     http_conn_id='AIRFLOW_CONN_SOLR_LEADER',
-    endpoint=param_endpoint_update,
+    endpoint=PARAM_ENDPOINT_UPDATE,
     data={"stream.body": "<delete><query>*:*</query></delete>"},
     headers={},
-    dag=dag)
+    dag=FTP_INDEX_DAG)
 
-solr_commit = SimpleHttpOperator(
+SOLR_COMMIT = SimpleHttpOperator(
     task_id='solr_commit',
     method='GET',
     http_conn_id='AIRFLOW_CONN_SOLR_LEADER',
-    endpoint=param_endpoint_update,
+    endpoint=PARAM_ENDPOINT_UPDATE,
     data={"stream.body": "<commit/>"},
     headers={},
-    dag=dag)
+    dag=FTP_INDEX_DAG)
 
 
-clear_index.set_upstream(pause_replication)
-solr_commit.set_upstream(clear_index)
-almasftp_task.set_upstream(solr_commit)
-resume_replication.set_upstream(almasftp_task)
+CLEAR_INDEX.set_upstream(PAUSE_REPLICATION)
+SOLR_COMMIT.set_upstream(CLEAR_INDEX)
+ALMASFTP_TASK.set_upstream(SOLR_COMMIT)
+RESUME_REPLICATION.set_upstream(ALMASFTP_TASK)

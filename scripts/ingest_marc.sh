c4d8cfc1caedda70b328b303796c02a13dd30fe7
David Kinzer <dtkinzer@gmail.com>
2019-04-02 11:22:27 -0400

    Assume rbenv is installed.
    When rbenv is installed it will use the .ruby version defined in
    tul_cob/.ruby-version

--- scripts/ingest_marc.sh
+++ scripts/ingest_marc.sh
@@ -1,16 +1,6 @@
 #!/bin/bash --login
 # Load RVM into a shell session *as a function*
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-  printf "First try to load from a user install"
-  source "$HOME/.rvm/scripts/rvm"
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-  printf "Then try to load from a root install"
-  source "/usr/local/rvm/scripts/rvm"
-else
-  printf "ERROR: An RVM installation was not found.\n"
-fi
 cd $HOME/tul_cob
-rvm use 2.4.1@tul_cob
 gem install bundler
 bundle install
 bundle exec traject -c lib/traject/indexer_config.rb ${1}

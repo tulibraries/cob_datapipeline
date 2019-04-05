4e964bd4763ae369766282319f9311012d395e26
Tim Bieniosek <tug76662@temple.edu>
2019-04-05 13:01:49 -0400

    No need to load rvm scripts if we're not using it

--- scripts/ingest_marc.sh
+++ scripts/ingest_marc.sh
@@ -1,14 +1,4 @@
 #!/bin/bash --login
-# Load RVM into a shell session *as a function*
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-  printf "First try to load from a user install"
-  source "$HOME/.rvm/scripts/rvm"
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-  printf "Then try to load from a root install"
-  source "/usr/local/rvm/scripts/rvm"
-else
-  printf "ERROR: An RVM installation was not found.\n"
-fi
 
 if [ ! -e ${1} ]
 then

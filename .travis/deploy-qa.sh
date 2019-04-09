76a763fdfe0e093a2291647321c7e62f73c8e864
Christina Harlow <cmharlow@gmail.com>
2019-04-09 15:33:59 -0400

    trying to get travis to see devops user ssh key

--- .travis/deploy-qa.sh
+++ .travis/deploy-qa.sh
@@ -2,8 +2,6 @@
 set -e
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
 
 cd ..
 git clone git@github.com/tulibraries/ansible-playbook-airflow.git # clone deployment playbook

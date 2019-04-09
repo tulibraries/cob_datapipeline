5d0a4bb5e9e1eb5a2e79881fc5905ec03e76e21e
Christina Harlow <cmharlow@gmail.com>
2019-04-09 15:00:37 -0400

    trying to get deploy step to run

--- .travis/deploy-master.sh
+++ .travis/deploy-master.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 set -e
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
 cd ..
-git clone git@github.com/tulibraries/ansible-playbook-airflow.git # clone deployment playbook
+git clone git@github.com:tulibraries/ansible-playbook-airflow.git # clone deployment playbook
 cd ansible-playbook-airflow
 sudo pip install pipenv # install playbook requirements
 pipenv install # install playbook requirements
-pipenv run ansible-galaxy -r requirements.yml # install playbook role requirements
+pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
 cp .circleci/.vault ~/.vault # setup vault password retrieval from travis envvar
 chmod +x ~/.vault  # setup vault password retrieval from travis envvar
 
 # deploy to qa using ansible-playbook
-pipenv run ansible-playbook -i inventory/stage/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229'
+pipenv run ansible-playbook -i inventory/stage/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private_key ~/.ssh/.conan_the_deployer
 EOF
--- .travis/deploy-qa.sh
+++ .travis/deploy-qa.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
 
-ssh vvv -T git@github.com
-
 cd ..
-git clone git@github.com/tulibraries/ansible-playbook-airflow.git # clone deployment playbook
+git clone git@github.com:tulibraries/ansible-playbook-airflow.git # clone deployment playbook
 cd ansible-playbook-airflow
 sudo pip install pipenv # install playbook requirements
 pipenv install # install playbook requirements
-pipenv run ansible-galaxy -r requirements.yml # install playbook role requirements
+pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
 cp .circleci/.vault ~/.vault # setup vault password retrieval from travis envvar
 chmod +x ~/.vault  # setup vault password retrieval from travis envvar
 
 # deploy to qa using ansible-playbook
-pipenv run ansible-playbook -i inventory/qa/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229'
+pipenv run ansible-playbook -i inventory/qa/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private_key ~/.ssh/.conan_the_deployer
 EOF

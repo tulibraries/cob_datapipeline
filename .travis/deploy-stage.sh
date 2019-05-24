bcb146b415cce5b82d00755b4a8b92b65a1b6633
cmharlow <cmharlow@gmail.com>
2019-05-24 15:39:53 -0400

    fixing erroroneous EOF in stage-deploy.sh script

--- .travis/deploy-stage.sh
+++ .travis/deploy-stage.sh
@@ -12,4 +12,3 @@ chmod +x ~/.vault  # setup vault password retrieval from travis envvar
 
 # deploy to qa using ansible-playbook
 pipenv run ansible-playbook -i inventory/stage/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private-key=~/.ssh/.conan_the_deployer
-EOF

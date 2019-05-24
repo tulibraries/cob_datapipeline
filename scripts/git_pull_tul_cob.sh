295442fbef2d12233376608bb68a4bf405d0090e
David Kinzer <dtkinzer@gmail.com>
2019-05-24 13:23:27 -0400

    Use fetch with reset --hard instead of pulling.
    pulling auto merges which can break if origin branch was rebased.

--- scripts/git_pull_tul_cob.sh
+++ scripts/git_pull_tul_cob.sh
@@ -9,6 +9,8 @@ if [ $1 == "true" ]; then
   latest=`git describe --tags $(git rev-list --tags --max-count=1)`
   git checkout $latest
 else
+  # TODO: we should not hard code master here...
+  git fetch origin master
   git checkout master
-  git pull origin master
+  git reset --hard FETCH_HEAD
 fi

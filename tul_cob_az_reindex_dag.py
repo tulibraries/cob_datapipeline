b4d6edc841b43dbc53316ff07ec2cc4b4341d58a
cmharlow <cmharlow@gmail.com>
2019-06-10 15:01:07 -0400

    run tasks w/o test env awareness (globals); linter repairs

--- tul_cob_az_reindex_dag.py
+++ tul_cob_az_reindex_dag.py
@@ -1,21 +1,21 @@
-import airflow
-from airflow import utils, settings, DAG
+"""Airflow DAG to index AZ Databases into Solr."""
 from datetime import datetime, timedelta
+import airflow
 from airflow.operators.python_operator import PythonOperator
 from cob_datapipeline.task_gitpulltulcob import task_git_pull_tulcob
 from cob_datapipeline.task_ingest_databases import get_database_docs, ingest_databases
 from cob_datapipeline.task_slackpost import task_az_slackpostonsuccess, task_slackpostonfail
 from cob_datapipeline.task_solrgetnumdocs import task_solrgetnumdocs
-from cob_datapipeline.globals import GIT_PULL_TULCOB_LATEST_RELEASE, GIT_PULL_TULCOB_BRANCH_NAME, AZ_CORE, AIRFLOW_CONN_SOLR_LEADER
 
-latest_release = GIT_PULL_TULCOB_LATEST_RELEASE
-git_ref = GIT_PULL_TULCOB_BRANCH_NAME
-solr_conn = AIRFLOW_CONN_SOLR_LEADER
+LATEST_RELEASE = airflow.models.Variable.get("GIT_PULL_TULCOB_LATEST_RELEASE")
+GIT_REF = airflow.models.Variable.get("GIT_PULL_TULCOB_BRANCH_NAME")
+AZ_CORE = airflow.models.Variable.get("AZ_CORE")
+SOLR_CONN = airflow.hooks.base_hook.BaseHook.get_connection("AIRFLOW_CONN_SOLR_LEADER")
 
 #
 # CREATE DAG
 #
-default_args = {
+DEFAULT_ARGS = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2019, 5, 28),
@@ -27,8 +27,8 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'tul_cob_az_reindex', default_args=default_args, catchup=False,
+AZ_DAG = airflow.DAG(
+    'tul_cob_az_reindex', default_args=DEFAULT_ARGS, catchup=False,
     max_active_runs=1, schedule_interval=None
 )
 
@@ -38,16 +38,16 @@ dag = DAG(
 # Tasks with all logic contained in a single operator can be declared here.
 # Tasks with custom logic are relegated to individual Python files.
 #
-get_num_solr_docs_pre = task_solrgetnumdocs(dag, AZ_CORE, 'get_num_solr_docs_pre')
-git_pull_tulcob_task = task_git_pull_tulcob(dag, latest_release, git_ref)
-get_database_docs_task = get_database_docs(dag)
-ingest_databases_task = ingest_databases(dag=dag, conn=solr_conn)
-get_num_solr_docs_post = task_solrgetnumdocs(dag, AZ_CORE, 'get_num_solr_docs_post')
+get_num_solr_docs_pre = task_solrgetnumdocs(AZ_DAG, AZ_CORE, 'get_num_solr_docs_pre')
+git_pull_tulcob_task = task_git_pull_tulcob(AZ_DAG, LATEST_RELEASE, GIT_REF)
+get_database_docs_task = get_database_docs(AZ_DAG)
+ingest_databases_task = ingest_databases(dag=AZ_DAG, conn=SOLR_CONN)
+get_num_solr_docs_post = task_solrgetnumdocs(AZ_DAG, AZ_CORE, 'get_num_solr_docs_post')
 post_slack = PythonOperator(
     task_id='slack_post_succ',
     python_callable=task_az_slackpostonsuccess,
     provide_context=True,
-    dag=dag
+    dag=AZ_DAG
 )
 
 #

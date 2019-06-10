7bdb8e2aba8554853fdcc376ab21ead2fdf1efab
cmharlow <cmharlow@gmail.com>
2019-06-10 15:01:07 -0400

    run tasks w/o test env awareness (globals); linter repairs

--- task_gitpulltulcob.py
+++ task_gitpulltulcob.py
@@ -1,11 +1,12 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow import AirflowException
-from cob_datapipeline.globals import AIRFLOW_HOME
+"""Airflow Task to clone TUL Cob GitHub Repository for Transform Code."""
 import os
+import airflow
+from airflow.operators.bash_operator import BashOperator
 
 def task_git_pull_tulcob(dag, latest_release, git_ref):
-    command = AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/git_pull_tul_cob.sh"
+    """Method that gathers required context then runs BashOperator for git pull."""
+    airflow_home = airflow.models.Variable.get("AIRFLOW_HOME")
+    command = airflow_home + "/dags/cob_datapipeline/scripts/git_pull_tul_cob.sh"
 
     if os.path.isfile(command):
         t1 = BashOperator(
--- task_ingest_databases.py
+++ task_ingest_databases.py
@@ -1,35 +1,40 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow import AirflowException
-from cob_datapipeline.globals import AIRFLOW_DATA_DIR, AIRFLOW_LOG_DIR, AIRFLOW_HOME, AZ_CORE, AZ_CLIENT_ID, AZ_CLIENT_SECRET
+"""Airflow Task to index AZ Database records into Solr."""
 import os
+import airflow
+from airflow.operators.bash_operator import BashOperator
 
-def ingest_databases(dag, conn, filename="", task_id="ingest_databases"):
+AIRFLOW_HOME = airflow.models.Variable.get("AIRFLOW_HOME")
+AIRFLOW_DATA_DIR = airflow.models.Variable.get("AIRFLOW_DATA_DIR")
+AIRFLOW_LOG_DIR = airflow.models.Variable.get("AIRFLOW_LOG_DIR")
+AZ_CORE = airflow.models.Variable.get("AZ_CORE")
+AZ_CLIENT_ID = airflow.models.Variable.get("AZ_CLIENT_ID")
+AZ_CLIENT_SECRET = airflow.models.Variable.get("AZ_CLIENT_SECRET")
+
+def ingest_databases(dag, conn, task_id="ingest_databases"):
     """Task for ingesting items
 
     Parameters:
         dag (airflow.models.Dag): The dag we will run this task in.
-        conn (airflow.models.connection): A Connection object representing the solr url we will ingest to.
-        filename (str): The name of the file that we will ingest from.
+        conn (airflow.models.connection): Connection object representing solr we index to.
         task_id (str): A label for this task.
 
     Returns:
         t1 (airflow.models.Task): The represention of this task.
     """
-    SOLR_URL = 'http://' + conn.host + ':' + str(conn.port) + '/solr/' + AZ_CORE
+    solr_url = 'http://' + conn.host + ':' + str(conn.port) + '/solr/' + AZ_CORE
     env = dict(os.environ)
     env.update({
-                "SOLR_AZ_URL": SOLR_URL,
-                "AIRFLOW_HOME": AIRFLOW_HOME,
-                "AIRFLOW_DATA_DIR": AIRFLOW_DATA_DIR,
-                "AIRFLOW_LOG_DIR": AIRFLOW_LOG_DIR,
-                })
+        "SOLR_AZ_URL": solr_url,
+        "AIRFLOW_HOME": AIRFLOW_HOME,
+        "AIRFLOW_DATA_DIR": AIRFLOW_DATA_DIR,
+        "AIRFLOW_LOG_DIR": AIRFLOW_LOG_DIR,
+    })
     return BashOperator(
-            task_id=task_id,
-            bash_command= AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/ingest_databases.sh ",
-            env=env,
-            dag=dag
-            )
+        task_id=task_id,
+        bash_command=AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/ingest_databases.sh ",
+        env=env,
+        dag=dag
+    )
 
 
 def get_database_docs(dag, task_id="get_database_docs"):
@@ -44,12 +49,12 @@ def get_database_docs(dag, task_id="get_database_docs"):
     """
     env = dict(os.environ)
     env.update({
-                "AZ_CLIENT_ID": AZ_CLIENT_ID,
-                "AZ_CLIENT_SECRET": AZ_CLIENT_SECRET,
-                })
+        "AZ_CLIENT_ID": AZ_CLIENT_ID,
+        "AZ_CLIENT_SECRET": AZ_CLIENT_SECRET,
+    })
     return BashOperator(
-            task_id=task_id,
-            bash_command= AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/get_database_docs.sh ",
-            env=env,
-            dag=dag,
-            )
+        task_id=task_id,
+        bash_command=AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/get_database_docs.sh ",
+        env=env,
+        dag=dag,
+    )

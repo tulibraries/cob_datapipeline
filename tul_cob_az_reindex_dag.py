a1f76a1612b5234cdce7b0d0cae69ac14072a42d
sensei100 <tuh42082@temple.edu>
2019-07-09 11:01:36 -0400

    wip-refactor az dag

--- tul_cob_az_reindex_dag.py
+++ tul_cob_az_reindex_dag.py
@@ -2,8 +2,7 @@
 from datetime import datetime, timedelta
 import airflow
 from airflow.operators.python_operator import PythonOperator
-from cob_datapipeline.task_gitpulltulcob import task_git_pull_tulcob
-from cob_datapipeline.task_ingest_databases import get_database_docs, ingest_databases
+from cob_datapipeline.task_ingest_databases import ingest_databases
 from cob_datapipeline.task_slackpost import task_az_slackpostonsuccess, task_slackpostonfail
 from cob_datapipeline.task_solrgetnumdocs import task_solrgetnumdocs
 
@@ -39,8 +38,6 @@ AZ_DAG = airflow.DAG(
 # Tasks with custom logic are relegated to individual Python files.
 #
 get_num_solr_docs_pre = task_solrgetnumdocs(AZ_DAG, AZ_CORE, 'get_num_solr_docs_pre')
-git_pull_tulcob_task = task_git_pull_tulcob(AZ_DAG, LATEST_RELEASE, GIT_REF)
-get_database_docs_task = get_database_docs(AZ_DAG)
 ingest_databases_task = ingest_databases(dag=AZ_DAG, conn=SOLR_CONN)
 get_num_solr_docs_post = task_solrgetnumdocs(AZ_DAG, AZ_CORE, 'get_num_solr_docs_post')
 post_slack = PythonOperator(
@@ -53,8 +50,6 @@ post_slack = PythonOperator(
 #
 # SET UP TASK DEPENDENCIES
 #
-git_pull_tulcob_task.set_upstream(get_num_solr_docs_pre)
-get_database_docs_task.set_upstream(git_pull_tulcob_task)
-ingest_databases_task.set_upstream(get_database_docs_task)
+ingest_databases_task.set_upstream(get_num_solr_docs_pre)
 get_num_solr_docs_post.set_upstream(ingest_databases_task)
 post_slack.set_upstream(get_num_solr_docs_post)

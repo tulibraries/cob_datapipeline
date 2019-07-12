84178610039300747129d320d8f902123c69e149
Tim Bieniosek <tug76662@temple.edu>
2019-07-12 14:27:52 -0400

    Update traject call to use new indexer repo

--- scripts/ingest_marc.sh
+++ scripts/ingest_marc.sh
@@ -19,7 +19,7 @@ then
   exit 1
 fi
 
-cd $HOME/tul_cob
+cd $HOME/cob_index
 gem install bundler
 bundle install
 SOLR_URL="$2" ALMAOAI_LAST_HARVEST_FROM_DATE="$3" bundle exec traject -c lib/traject/indexer_config.rb ${1}

22c48ea6d4c6de0ca46406c686710a5fb8a8002e
Tim Bieniosek <tug76662@temple.edu>
2019-08-20 13:52:33 -0400

    Fix the indexer_config.rb path for ingest_multi

--- scripts/ingest_marc_multi.sh
+++ scripts/ingest_marc_multi.sh
@@ -17,6 +17,6 @@ gem install bundler
 bundle install
 for f in $1/alma_bibs__*.xml
 do
-TRAJECT_FULL_REINDEX='yes' SOLR_URL="$2" bundle exec traject -c lib/traject/indexer_config.rb $f
+TRAJECT_FULL_REINDEX='yes' SOLR_URL="$2" bundle exec traject -c lib/cob_index/indexer_config.rb $f
 done
 return 0

1da46119d11ed5e0e223a83cf6fbff08ab12ce03
David Kinzer <dtkinzer@gmail.com>
2019-08-22 17:13:25 -0400

    Remove almafullreindex_inprogress variable.
    Setting this variable in task creation is causing it to get recreated
    every time we try to edit it.  And, this seems to be causing an error
    where task fails under certain conditions.

--- task_almasftp.py
+++ task_almasftp.py
@@ -1,13 +1,11 @@
 from airflow import DAG
 from airflow import AirflowException
-from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
 import os
 from cob_datapipeline.almasftp_fetch import almasftp_fetch
 
 
 def task_almasftp(dag):
-    Variable.set("almafullreindex_inprogress", True)
     t1 = PythonOperator(
         task_id='almasftp',
         python_callable=almasftp_fetch,

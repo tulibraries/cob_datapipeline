41c5c8c3ca9db5f0fc0bee8825b6540f7e12c850
Tim Bieniosek <tug76662@temple.edu>
2019-09-03 09:12:57 -0400

    Make sure the BashOperator gets the return value of ingest_marc.sh. Make ingest_marc.sh catch more error conditions.

--- scripts/ingest_marc.sh
+++ scripts/ingest_marc.sh
@@ -2,7 +2,10 @@
 # Argument 1 is the MARCXML file to ingest
 # Argument 2 is the SOLR URL
 # Argument 3 is the harvest-from date
+# Argument 4 is the Traject log file for future parsing
 source $HOME/.bashrc
+# print some information for future debugging
+set -ux
 echo $1
 echo $2
 echo $3
@@ -14,13 +17,17 @@ which gem
 which bundle
 ruby -v
 
+# check that the MARC XML file exists
 if [ ! -e ${1} ]
 then
   exit 1
 fi
 
-cd $HOME/cob_index
-gem install bundler
-bundle install
- SOLR_URL="$2" ALMAOAI_LAST_HARVEST_FROM_DATE="$3" bundle exec traject -c lib/cob_index/indexer_config.rb ${1}
-return 0
+# make sure the indexer config exists
+cd $HOME/cob_index || exit 1
+gem install bundler || exit 1
+bundle install || exit 1
+# ingest the MARC XML, teeing the output to a separate log file
+{
+SOLR_URL="$2" ALMAOAI_LAST_HARVEST_FROM_DATE="$3" bundle exec traject -c lib/cob_index/indexer_config.rb ${1}
+}  2>&1 | tee ${4}

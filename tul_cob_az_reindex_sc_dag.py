33d4d3bd90411fe2e4d066bfef259dd210456613
David Kinzer <dtkinzer@gmail.com>
2019-09-03 10:40:40 -0400

    wip - create solrcloud az dag

---
+++ tul_cob_az_reindex_sc_dag.py
@@ -0,0 +1,66 @@
+"""Airflow DAG to index AZ Databases into Solr."""
+from datetime import datetime, timedelta
+import airflow
+from airflow.operators.python_operator import PythonOperator
+from cob_datapipeline.task_ingest_databases import ingest_databases
+from cob_datapipeline.task_slackpost import task_az_slackpostonsuccess, task_slackpostonfail
+from cob_datapipeline.task_sc_get_num_docs import task_solrgetnumdocs
+
+#
+# INIT SYSTEMWIDE VARIABLES
+#
+# check for existence of systemwide variables shared across tasks that can be
+# initialized here if not found (i.e. if this is a new installation) & defaults exist
+#
+
+# Get Solr URL & Collection Name for indexing info; error out if not entered
+SOLR_CONN = airflow.hooks.base_hook.BaseHook.get_connection("SOLRCLOUD")
+CONFIGSET = Variable.get("AZ_CONFIGSET")
+COLLECTION = CONFIGSET + "-" + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+SOLR_ENDPOINT = "http://" + SOLR_CONN.host + ":" + str(SOLR_CONN.port) + "/solr/" + COLLECTION
+
+AZ_INDEX_SCHEDULE_INTERVAL = airflow.models.Variable.get("AZ_INDEX_SCHEDULE_INTERVAL")
+#
+# CREATE DAG
+#
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 5, 28),
+    'email': ['david.kinzer@temple.edu'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': task_slackpostonfail,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+}
+
+AZ_DAG = airflow.DAG(
+    'tul_cob_az_sc_reindex', default_args=DEFAULT_ARGS, catchup=False,
+    max_active_runs=1, schedule_interval=AZ_INDEX_SCHEDULE_INTERVAL
+)
+
+#
+# CREATE TASKS
+#
+# Tasks with all logic contained in a single operator can be declared here.
+# Tasks with custom logic are relegated to individual Python files.
+#
+
+
+get_num_solr_docs_pre = task_solrgetnumdocs(AZ_DAG, CONFIGSET, 'get_num_solr_docs_pre', conn_id=SOLR_CONN.id)
+ingest_databases_task = ingest_databases(dag=AZ_DAG, conn=SOLR_CONN)
+get_num_solr_docs_post = task_solrgetnumdocs(AZ_DAG, CONFIGSET, 'get_num_solr_docs_post', conn_id=SOLRCLOUD.id)
+post_slack = PythonOperator(
+    task_id='slack_post_succ',
+    python_callable=task_az_slackpostonsuccess,
+    provide_context=True,
+    dag=AZ_DAG
+)
+
+#
+# SET UP TASK DEPENDENCIES
+#
+ingest_databases_task.set_upstream(get_num_solr_docs_pre)
+get_num_solr_docs_post.set_upstream(ingest_databases_task)
+post_slack.set_upstream(get_num_solr_docs_post)

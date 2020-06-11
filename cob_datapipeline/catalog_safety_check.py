c36998e4c2fe46f614261315461f746e2c9a9775
David Kinzer <dtkinzer@gmail.com>
2020-06-11 12:24:20 -0400

    Fix undefined CATALOG_PRE_PRODUCTION_SOLR_COLLECTION.
    * Use None as default when running safety check.

--- cob_datapipeline/catalog_safety_check.py
+++ cob_datapipeline/catalog_safety_check.py
@@ -1,8 +1,8 @@
 from airflow.models import Variable
 
 def safety_check(**context):
-    pre_prod_collection = Variable.get("CATALOG_PRE_PRODUCTION_SOLR_COLLECTION")
-    prod_collection = Variable.get("CATALOG_PRODUCTION_SOLR_COLLECTION")
+    pre_prod_collection = Variable.get("CATALOG_PRE_PRODUCTION_SOLR_COLLECTION", None)
+    prod_collection = Variable.get("CATALOG_PRODUCTION_SOLR_COLLECTION", None)
 
-    if pre_prod_collection == prod_collection:
+    if pre_prod_collection == prod_collection and pre_prod_collection != None:
         raise Exception("The pre production collection cannot be equal to the production collection.")

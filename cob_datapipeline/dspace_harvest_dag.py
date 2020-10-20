9ea0552c4a34d28f20ba89537c61369c10b98ba4
sensei100 <tuh42082@temple.edu>
2020-10-20 14:06:00 -0400

    BL-1344 Harvest task
    - Adds harvest task

--- cob_datapipeline/dspace_harvest_dag.py
+++ cob_datapipeline/dspace_harvest_dag.py
@@ -1,7 +1,30 @@
 """Airflow DAG to harvest DSpace electronic theses and dissertations"""
 from datetime import datetime, timedelta
-from tulflow import tasks
+from tulflow import harvest, tasks
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
 import airflow
+
+"""
+INIT SYSTEMWIDE VARIABLES
+check for existence of systemwide variables shared across tasks that can be
+initialized here if not found (i.e. if this is a new installation)
+"""
+
+# Get S3 data bucket variables
+AIRFLOW_S3 = BaseHook.get_connection("AIRFLOW_S3")
+AIRFLOW_DATA_BUCKET = Variable.get("AIRFLOW_DATA_BUCKET")
+S3_NAME_SPACE = '{{ execution_date.strftime("%Y-%m-%d_%H-%M-%S") }}'
+
+# OAI Harvest Variables
+DSPACE_HARVEST_FROM_DATE = Variable.get("DSPACE_HARVEST_FROM_DATE")
+DEFAULT_HARVEST_UNTIL_DATE = '{{ execution_date.strftime("%Y-%m-%dT%H:%M:%SZ") }}'
+DSPACE_HARVEST_UNTIL_DATE = Variable.get("DSPACE_HARVEST_UNTIL_DATE", default_var=DEFAULT_HARVEST_UNTIL_DATE)
+DSPACE_OAI_CONFIG = Variable.get("DSPACE_OAI_CONFIG", deserialize_json=True)
+DSPACE_OAI_MD_PREFIX = DSPACE_OAI_CONFIG.get("md_prefix")
+DSPACE_OAI_ENDPOINT = DSPACE_OAI_CONFIG.get("endpoint")
+DSPACE_OAI_SETSPEC = DSPACE_OAI_CONFIG.get("setspec")
 
 # CREATE DAG
 DEFAULT_ARGS = {
@@ -28,3 +51,22 @@ CREATE TASKS
 Tasks with all logic contained in a single operator can be declared here.
 Tasks with custom logic are relegated to individual Python files.
 """
+
+OAI_HARVEST = PythonOperator(
+    task_id='oai_harvest',
+    provide_context=True,
+    python_callable=harvest.oai_to_s3,
+    op_kwargs={
+        "access_id": AIRFLOW_S3.login,
+        "access_secret": AIRFLOW_S3.password,
+        "bucket_name": AIRFLOW_DATA_BUCKET,
+        "harvest_from_date": DSPACE_HARVEST_FROM_DATE,
+        "harvest_until_date": DSPACE_HARVEST_UNTIL_DATE,
+        "metadata_prefix": DSPACE_OAI_MD_PREFIX,
+        "oai_endpoint": DSPACE_OAI_ENDPOINT,
+        "setspec": DSPACE_OAI_SETSPEC,
+        "records_per_file": 1000,
+        "timestamp": f"{ S3_NAME_SPACE }"
+    },
+    dag=DAG
+)
--- tests/dspace_harvest_test.py
+++ tests/dspace_harvest_test.py
@@ -1,9 +1,6 @@
 """Unit Tests for the DSpace harvest DAG"""
-from datetime import datetime, timedelta
 import unittest
-from tulflow import tasks
 import airflow
-
 from cob_datapipeline.dspace_harvest_dag import DAG
 
 class TestDspaceHarvestDag(unittest.TestCase):
@@ -12,3 +9,19 @@ class TestDspaceHarvestDag(unittest.TestCase):
     def setUp(self):
         """Method to set up the DAG Class instance for testing."""
         self.tasks = list(map(lambda t: t.task_id, DAG.tasks))
+
+    def test_dag_loads(self):
+        """Unit test that the DAG identifier is set correctly."""
+        self.assertEqual(DAG.dag_id, "dspace_harvest")
+
+    def test_dag_tasks_present(self):
+        """Unit test that the DAG instance contains the expected tasks."""
+        self.assertEqual(self.tasks, [
+            "oai_harvest",
+            ])
+
+    def test_oai_harvest_task(self):
+        """Unit test that oai_harvest dag has kwargs."""
+        task = DAG.get_task("oai_harvest")
+        self.assertEqual(task.op_kwargs["bucket_name"], "test_bucket")
+        self.assertEqual(task.op_kwargs["oai_endpoint"], "foobar")

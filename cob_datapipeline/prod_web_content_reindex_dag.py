1659c793c8e15748220b01fef8f232377b3d7439
sensei100 <tuh42082@temple.edu>
2023-10-24 13:51:55 -0400

    Fix template syntax

--- cob_datapipeline/prod_web_content_reindex_dag.py
+++ cob_datapipeline/prod_web_content_reindex_dag.py
@@ -12,7 +12,7 @@ from cob_datapipeline.operators import\
         PushVariable, DeleteAliasListVariable, DeleteCollectionListVariable
 from airflow.providers.slack.notifications.slack import send_slack_notification
 
-slackpostonsuccess = send_slack_notification(channel="blacklight_project", username="airflow", text=":partygritty: {{ execution_date }} DAG {{ dag.dag_id }} success: We started with {{ ti.xcom_pull(task_ids='get_num_solr_docs_pre'), key='return_value'['response']['numFound'] }} and ended with {{ ti.xcom_pull(task_ids='get_num_solr_docs_post')['response']['numFound'] }} docs. {{ ti.log_url }}")
+slackpostonsuccess = send_slack_notification(channel="blacklight_project", username="airflow", text=":partygritty: {{ execution_date }} DAG {{ dag.dag_id }} success: We started with {{ ti.xcom_pull(task_ids='get_num_solr_docs_pre', key='return_value'['response']['numFound']) }} and ended with {{ ti.xcom_pull(task_ids='get_num_solr_docs_post',['response']['numFound']) }} docs. {{ ti.log_url }}")
 slackpostonfail = send_slack_notification(channel="infra_alerts", username="airflow", text=":poop: Task failed: {{ dag.dag_id }} {{ ti.task_id }} {{ execution_date }} {{ ti.log_url }}")
 
 

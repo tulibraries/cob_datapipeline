6aa5c40314ac2c17ace15380031027f92eedcd57
Jen <tuh42082@temple.edu>
2025-01-22 13:42:41 -0500

    Update bundle config command (#1659)

--- cob_datapipeline/scripts/ingest_databases.sh
+++ cob_datapipeline/scripts/ingest_databases.sh
@@ -17,6 +17,6 @@ source $HOME/.bashrc
 set -e
 git clone https://github.com/tulibraries/cob_az_index.git --branch=$AZ_BRANCH
 cd cob_az_index
-bundle config build.nokogiri --use-system-libraries
+bundle config set force_ruby_platform true
 bundle install --without=debug
 bundle exec cob_az_index ingest $AZ_DELETE_SWITCH

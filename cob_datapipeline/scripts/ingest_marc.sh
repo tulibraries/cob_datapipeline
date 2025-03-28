5215f629eeac26446b9f767d56b88d5ea75abd2d
David Kinzer <dtkinzer@gmail.com>
2025-03-28 10:39:57 -0400

    Revert "update ingest script to handle big lists (#1680)"
    This reverts commit c4bc2cb6013961336536286e3573f707be4a4aeb.

--- cob_datapipeline/scripts/ingest_marc.sh
+++ cob_datapipeline/scripts/ingest_marc.sh
@@ -18,14 +18,14 @@ gem install bundler
 bundle config set force_ruby_platform true
 bundle install --without=debug
 
+data_in=$(echo $DATA | jq -r '.[]')
+
 if [ -z "$COMMAND" ]; then
   COMMAND=ingest
 fi
 
-ingest_marc_file() {
-  set -eo pipefail  # Ensure failures stop execution inside this function
-  cd tmp/cob_index || exit 1
-  file="$1"
+for file in $data_in
+do
   echo "Indexing file: "$file
   bundle exec cob_index $COMMAND $(aws s3 presign s3://$BUCKET/$file)
   processed_file=$(echo $file | sed 's/new-updated/processed-new-updated/' | sed 's/deleted/processed-deleted/')
@@ -34,15 +34,4 @@ ingest_marc_file() {
   if [ "$file" != "$processed_file" ]; then
     aws s3 mv s3://$BUCKET/$file s3://$BUCKET/$processed_file
   fi
-}
-
-export -f ingest_marc_file
-export COMMAND
-export BUCKET
-export SOLR_AUTH_USER
-export SOLR_AUTH_PASSWORD
-export SOLR_URL
-export ALMAOAI_LAST_HARVEST_FROM_DATE
-
-echo "$DATA" | jq -r '.[]' | xargs -n 1 -I {} bash -c 'igest_marc_file "{}"'
-
+done

a3ee88d58b95fa394c81b681fc9d9b63d0118dae
Steven Ng <steven.ng@temple.edu>
2025-08-15 08:48:30 -0600

    Remove explicitly set AIRFLOW_HOME
    - Vestage of developoment

--- cob_datapipeline/gencon_ingest_dag.py
+++ cob_datapipeline/gencon_ingest_dag.py
@@ -64,8 +64,6 @@ Tasks with all logic contained in a single operator can be declared here.
 Tasks with custom logic are relegated to individual Python files.
 """
 
-
-AIRFLOW_HOME = "/opt/airflow"
 INDEX_GENCON = BashOperator(
     task_id="index_gencon",
     bash_command=AIRFLOW_HOME + "/dags/cob_datapipeline/scripts/ingest_gencon.sh ",
